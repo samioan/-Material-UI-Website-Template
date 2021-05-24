f3c40f781bd70d7958bd587facc4eb1d3a72de66
samioan <ioannis.siampalias@gmail.com>
2021-05-24 21:20:26 +0300

    npm modules fix

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,6 @@ import styles from "./styles";
 import Grid from "@material-ui/core/Grid";
 import linkDetails from "./util/linkDetails";
 import Button from "@material-ui/core/Button";
-import icon from "./images/icon.jpg";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import imageCarousel from "./util/imageCarousel";
