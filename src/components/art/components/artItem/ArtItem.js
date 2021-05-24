84ad67c6acc65bf8a22c6270b66659b52bd539a0
Ioannis Siampalias <60889639+samioan@users.noreply.github.com>
2021-05-24 21:12:40 +0300

    Revert "Game pages"

--- src/components/art/components/artItem/ArtItem.js
+++ src/components/art/components/artItem/ArtItem.js
@@ -1,13 +1,30 @@
 import React from "react";
-import styles from "./styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 
-const ArtItem = ({ image, title, link }) => {
-  const classes = styles();
+const useStyles = makeStyles({
+  heading1: {
+    color: "#fff",
+  },
+  root: {
+    backgroundColor: "#111",
+  },
+  cardContent: {
+    minWidth: 300,
+  },
+  media: {
+    height: 400,
+  },
+});
+
+export default function ArtItem({ image, title, link }) {
+  const classes = useStyles();
 
   return (
     <Link
@@ -17,11 +34,11 @@ const ArtItem = ({ image, title, link }) => {
       target="_blank"
       style={{ textDecoration: "none" }}
     >
-      <Card className={classes.card}>
+      <Card className={classes.root}>
         <CardMedia className={classes.media} image={image} title={title} />
         <CardContent className={classes.cardContent}>
           <Typography
-            className={classes.title}
+            className={classes.heading1}
             gutterBottom
             variant="h5"
             component="h2"
@@ -29,10 +46,13 @@ const ArtItem = ({ image, title, link }) => {
             {title}
           </Typography>
         </CardContent>
+
+        <CardActions>
+          <Button target="_blank" href={link} size="small" color="primary">
+            Full Image
+          </Button>
+        </CardActions>
       </Card>
     </Link>
   );
-};
-
-export { ArtItem };
-export default ArtItem;
+}
