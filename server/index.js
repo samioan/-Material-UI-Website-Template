89e8f2bcdde5c16a0eb163c2128c1432be02dba6
samioan <ioannis.siampalias@gmail.com>
2022-05-31 20:28:07 +0300

    new server test

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(process.env.PORT || 5000, () => console.log("Server Running"));
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
 
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
