f1bb22b5b78f042e88bb1237c27a1cea0483e63c
samioan <ioannis.siampalias@gmail.com>
2022-05-31 20:45:32 +0300

    catch all route function

--- server/index.js
+++ server/index.js
@@ -52,3 +52,11 @@ router.post("/contact", (req, res) => {
     }
   });
 });
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "build/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
